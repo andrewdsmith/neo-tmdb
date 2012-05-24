e2e6d758b311fdf0e89eb7a6cc9fa399a7aedd0b
Andrew Smith <andrew.smith@neworbit.org>
2012-05-24 21:35:17 +0100

    Add API key loading from environment for rspec
    Ultimately we'll use something like vcr so that anyone can re-run these
    tests without having an API key, although why anyone would want to
    develop this library without an API key is beyond me.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,7 @@
 require 'neo-tmdb'
+
+api_key = ENV["TMDB_API_KEY"] or raise "You must set the TMDB_API_KEY environment variable to run these tests."
+
+TMDb.configure do |config|
+  config.api_key = api_key
+end
