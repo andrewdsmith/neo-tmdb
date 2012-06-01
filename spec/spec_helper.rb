15ffb13ee9dd67f0a93786c4b01ff704f31c7179
Andrew Smith <andrew.smith@neworbit.org>
2012-05-31 21:41:25 -0400

    Add basic vcr config to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,16 @@
 require 'neo-tmdb'
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/cassettes'
+  config.hook_into :faraday
+  config.configure_rspec_metadata!
+end
+
+RSpec.configure do |config|
+  # In RSpec 3 this will no longer be necessary.
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+end
 
 api_key = ENV["TMDB_API_KEY"] or raise "You must set the TMDB_API_KEY environment variable to run these tests."
 
