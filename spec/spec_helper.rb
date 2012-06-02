d9e6c32af1cd08680cb1d94a8fb54af04e2fed4a
Andrew Smith <andrew.smith@neworbit.org>
2012-06-01 20:59:16 -0400

    Filter api_key in vcr cassette requests
    Each person running the tests will have their own TMDb API key but the response
    will be the same. By filtering the api_key parameter we can let everyone reuse
    the same cassettes or even run the tests without a key. Additionally, we don't
    want to disclose any private keys in the committed cassettes.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :faraday
   config.configure_rspec_metadata!
+  config.filter_sensitive_data('TMDB_API_KEY') { TMDb.configuration.api_key }
 end
 
 RSpec.configure do |config|
@@ -12,8 +13,10 @@ RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
 end
 
-api_key = ENV["TMDB_API_KEY"] or raise "You must set the TMDB_API_KEY environment variable to run these tests."
-
 TMDb.configure do |config|
-  config.api_key = api_key
+  # In the absense of the TMDB_API_KEY environment variable we have to use a
+  # fake string because vcr refuses to perform placeholder substitution if
+  # given a blank.
+  key = ENV['TMDB_API_KEY']
+  config.api_key = key.nil? || key.empty? ? 'fake-tmdb-api-key' : key
 end
