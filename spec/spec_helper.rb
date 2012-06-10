739670f5831220217ebeee91629876d5806ef499
Andrew Smith <andrew.smith@neworbit.org>
2012-06-10 20:51:51 +0100

    Add support for :focus metadata filtering in rspec
    This is just a convenience to allow isolated tests to be run by adding
    :focus to the example or example group metadata.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,9 @@ VCR.configure do |config|
 end
 
 RSpec.configure do |config|
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
+
   # In RSpec 3 this will no longer be necessary.
   config.treat_symbols_as_metadata_keys_with_true_values = true
 end
