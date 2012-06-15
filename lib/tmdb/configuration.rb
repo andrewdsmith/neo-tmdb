cec311e5f2cf8cd312f76e44652f8988320a1b85
Andrew Smith <andrew.smith@neworbit.org>
2012-06-15 13:55:01 +0100

    Add Configuration methods returning image sizes

--- lib/tmdb/configuration.rb
+++ lib/tmdb/configuration.rb
@@ -2,9 +2,34 @@ module TMDb
   class Configuration
     attr_accessor :api_key
 
+    # Returns the base URL for use in constructing image URLs.
     def images_base_url
+      tmdb_config_images['base_url']
+    end
+
+    # Returns the list of film backdrop image sizes for use in constructing
+    # image URLs.
+    def images_backdrop_sizes
+      tmdb_config_images['backdrop_sizes']
+    end
+
+    # Returns the list of film poster image sizes for use in constructing image
+    # URLs.
+    def images_poster_sizes
+      tmdb_config_images['poster_sizes']
+    end
+
+    # Returns the list of person profile image sizes for use in constructing
+    # image URLs.
+    def images_profile_sizes
+      tmdb_config_images['profile_sizes']
+    end
+
+    protected
+
+    def tmdb_config_images
       @tmdb_config ||= TMDb.get_api_response('configuration')
-      @tmdb_config['images']['base_url']
+      @tmdb_config['images']
     end
   end
 end
--- spec/tmdb/configuration_spec.rb
+++ spec/tmdb/configuration_spec.rb
@@ -11,23 +11,30 @@ module TMDb
       end
     end
 
-    context "when configured with an API key value" do
-      before(:each) do
-        # See spec/spec_helper.rb for setup of the API key.
-        config.api_key = TMDb.configuration.api_key
-      end
-      describe "#images_base_url", :vcr => { :cassette_name => "configuration" } do
-        it "returns the base URL for images" do
-          config.images_base_url.should == "http://cf2.imgobject.com/t/p/"
+    shared_examples :config_reader do |method, expected_value|
+      describe "##{method}" do
+        it "returns the #{ method.to_s.gsub('_', ' ') }" do
+          config.send(method).should == expected_value
         end
-        it "only hits the url once" do
+        it "caches the result for subsequent calls" do
           # This test currently works because VCR will refuse the second
           # request when replaying the cassette recorded in the first example.
           # This means that if this example will pass if run in isolation or
           # before other examples and vcr is re-recording the cassette.
-          expect { 2.times { config.images_base_url } }.to_not raise_error
+          expect { 2.times { config.send(method) } }.to_not raise_error
         end
       end
+    end
+
+    context "when configured with an API key value", :vcr => { :cassette_name => "configuration" } do
+      before(:each) do
+        # See spec/spec_helper.rb for setup of the API key.
+        config.api_key = TMDb.configuration.api_key
+      end
+      include_examples :config_reader, :images_backdrop_sizes, ['w300', 'w780', 'w1280', 'original']
+      include_examples :config_reader, :images_base_url, 'http://cf2.imgobject.com/t/p/'
+      include_examples :config_reader, :images_poster_sizes,  ["w92", "w154", "w185", "w342", "w500", "original"]
+      include_examples :config_reader, :images_profile_sizes,  ["w45", "w185", "h632", "original"]
     end
   end
 end
