3c472215dbd7a2131022526d68fca6fd7df24c3e
Andrew Smith <andrew.smith@neworbit.org>
2012-07-10 22:20:27 +0100

    Add vcr support for scenarios
    Replace aruba-supplied steps in scenario with customised step, allow us
    to wrap the executed code in vcr configuration and cassette management
    code. This seems quite novel as I couldn't find an example of mixing
    vcr and aruba this way.
    
    Includes the vcr cassette for the scenario. This seemed sufficiently
    different to prior recordings for the specs (primarily the content being
    formatted differently) that I decided to include this new recording.
    
    Also simplifies the step relating to environment variable which was
    needlessly parameterised, inviting failures in future if a different
    variable name was used in future scenarios.

--- features/step_definitions/tmdb_steps.rb
+++ features/step_definitions/tmdb_steps.rb
@@ -1,5 +1,27 @@
-Given /^my TMDb API key in environment variable "(.*?)"$/ do |name|
-  unless ENV[name]
-    fail "To run this scenario environment variable '#{name}' must contain a valid TMDb API key."
+Before do |scenario|
+  @vcr_cassette = "#{scenario.feature.name.split("\n").first}/#{scenario.name}".downcase
+end
+
+Given /^my TMDb API key in environment variable TMDB_API_KEY$/ do
+  unless (ENV['TMDB_API_KEY'])
+    fail "To run this scenario environment variable 'TMDB_API_KEY' must contain a valid TMDb API key."
   end
+end
+
+When /^I run the following code:$/ do |code|
+  code = %{
+    require 'vcr'
+    VCR.configure do |c|
+      c.hook_into :faraday
+      c.cassette_library_dir = '../../features/cassettes'
+      c.default_cassette_options = {:record => :new_episodes }
+      c.filter_sensitive_data('TMDB_API_KEY') { ENV['TMDB_API_KEY'] }
+    end
+
+    VCR.use_cassette('#{@vcr_cassette}') do
+      #{code}
+    end
+  }
+  write_file('scenario.rb', code)
+  run_simple('ruby scenario.rb')
 end
