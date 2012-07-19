8cdfce885c6d1dbef6ebcdd83b046b558a004299
Andrew Smith <andrew.smith@neworbit.org>
2012-07-19 22:22:59 +0100

    Make scenarios runnable without an API key
    This resurrects the scheme implemented previously for the specs, so that
    we can run the scenarios without an API key, which we need for Travis CI
    builds.

--- features/step_definitions/tmdb_steps.rb
+++ features/step_definitions/tmdb_steps.rb
@@ -3,9 +3,12 @@ Before do |scenario|
 end
 
 Given /^my TMDb API key in environment variable TMDB_API_KEY$/ do
-  unless (ENV['TMDB_API_KEY'])
-    fail "To run this scenario environment variable 'TMDB_API_KEY' must contain a valid TMDb API key."
-  end
+  # We cannot verify the environement variable here as it's only truely
+  # required when we're recording a vcr cassette. The vcr configuration code
+  # intercepts real requests and checks that the environment variable is set.
+  # This way, it's possible to run the the scenarios without a TMDb API key
+  # using the vcr cassettes. This is useful in a public CI environment like
+  # Travis, where we don't want to expose a key.
 end
 
 Given /^the TMDb service is unavailable$/ do
@@ -21,13 +24,27 @@ end
 When /^I run the following code:$/ do |code|
   @code_template ||= %!
     require 'vcr'
+
+    # In the absense of the TMDB_API_KEY environment variable we have to use a
+    # fake string because vcr refuses to perform placeholder substitution if
+    # given a blank.
+    ENV['TMDB_API_KEY'] = 'fake_tmdb_api_key' unless ENV['TMDB_API_KEY']
+
     VCR.configure do |c|
       c.hook_into :faraday
       c.cassette_library_dir = '../../features/cassettes'
       c.default_cassette_options = { :record => :new_episodes }
       c.filter_sensitive_data('TMDB_API_KEY') { ENV['TMDB_API_KEY'] }
       c.before_http_request do |request|
+        # Log requests for verification in later steps.
         STDERR.puts "Request made to \#{request.uri}"
+      end
+      c.before_http_request(:real?) do |request|
+        if TMDb.configuration.api_key == 'fake_tmdb_api_key'
+         raise "You cannot run this test without setting the TMDB_API_KEY " +
+           "environment variable to your TMDb API key as a real HTTP request " +
+           "to the TMDb must be made."
+        end
       end
     end
 
